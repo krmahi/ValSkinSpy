6f872522107c049394cebe5e18969164ee276262
s3Em <madhuripatil0015@gmail.com>
2024-11-18 12:26:12 +0530

    Get Access token & Entitlement token

---
+++ getTokens.py
@@ -0,0 +1,37 @@
+import requests
+import urllib3
+import base64
+
+file1 = open("C:\\Users\\Em\\AppData\\Local\\Riot Games\\Riot Client\\Config\\lockfile", "r")
+val = file1.read().split(':')
+
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+url = f"https://127.0.0.1:{val[2]}/entitlements/v1/token"
+
+print("port:", val[2])
+
+encoded = base64.b64encode(f"riot:{val[3]}".encode())
+ 
+payload = ""    
+headers = {
+    "User-Agent": "insomnia/10.1.1",
+    # "User-Agent": "Mozilla/5.0",
+    # "Authorization": "Basic cmlvdDpNUlBoc3cxTlN1UFJEQnpBYmNxNm1nCg=="
+    "Authorization": "Basic" + " " + str(encoded)[2:-1]
+}
+
+print("headers:", headers)
+
+response = requests.get(url,data=payload, headers=headers, verify=False)
+
+if response.status_code == 200:
+    response_json = response.json()
+    access_token = response_json.get("accessToken")
+    entitlements_token = response_json.get("token")
+
+
+# print(response.text)
+print("Access Token: Bearer", access_token)
+print("\n")
+print("Entitlements Token:", entitlements_token)
